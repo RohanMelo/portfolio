5a88e6b91d706d974096a3c98a18969c77a55647
Rodrigo Melo <rodrigojin@gmail.com>
2022-03-07 13:19:58 -0300

    Changed header

--- src/components/HeaderGeneric.js
+++ src/components/HeaderGeneric.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderGeneric = (props) => (
     <header id="header">
         <h1>Relevant Projects</h1>
-        <p>Click on titles to go to the respective Github Repo<br />
+        <p>Click on titles to go to the respective Github Repo or site<br />
         Click on image thumbnails to enlarge them</p>
     </header>
 )
